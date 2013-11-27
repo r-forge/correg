8f56c9d2739d9d9bd95b45d670b6d0ee9600da05
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-27 17:41:33 +0000

    some doc upgrade
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/qui_nom.R
+++ pkg/CorReg/R/qui_nom.R
@@ -1,4 +1,5 @@
 #' compare les nomes pr?sents dans deux vecteurs et les identifie
+#' @useDynLib CorReg
 qui_nom<-function(nomcompl=nomcompl,nom=nom){
   loc=c(nom,nomcompl)
   return(which(duplicated(loc))-length(nom))
