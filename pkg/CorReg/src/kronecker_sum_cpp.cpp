c9a8aed6273bbe3dd91b700d6806161be09a2d7d
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-27 15:30:26 +0000

    translate comments
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/src/kronecker_sum_cpp.cpp
+++ pkg/CorReg/src/kronecker_sum_cpp.cpp
@@ -3,15 +3,12 @@
 using namespace Rcpp ;
 using namespace Eigen;
 using namespace std;
-//using Eigen::Map;
-//using Eigen::MatrixXd;
-//using Rcpp::as;
-//V1 et V2 sont des vecteurs colonnes issus de matrices (sous-matrices donc)
+
+//V1 and V2 are column vectors from matrices (sub-matrices)
 RcppExport Rcpp::NumericVector kronecker_sum_cpp(const Rcpp::NumericVector &V1,const Rcpp::NumericVector &V2){
     int size1=V1.size();
     int size2=V2.size();
-    int size3=size1*size2;
-    
+    int size3=size1*size2;   
     Rcpp::NumericVector V3(size3);
     int ou=0;
     for(int i=0;i<size1;i++){
