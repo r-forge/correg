d0eb6d1a98034e613cec8ccb407c33bdf18cf8be
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-11-29 08:56:11 +0000

    translate some parameters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@43 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/BicZ.R
+++ pkg/CorReg/R/BicZ.R
@@ -2,20 +2,20 @@
 #' @export
 #' @param X the dataset
 #' @param Z the structure
-#' @param Bic_vide_vect the BIC of the null hypothesis (used for independent variables)
+#' @param Bic_null_vect the BIC of the null hypothesis (used for independent variables)
 #' @param BicOld BIC (vector) associated to Zold
 #' @param Zold another structure with some common parts with Z (allows to compute only the differences)
 #' @param methode parameter for OLS 
-BicZ<-function(X=X,Z=Z,Bic_vide_vect=NULL,BicOld=NULL,methode=1,Zold=NULL){
- if(is.null(Bic_vide_vect)){
+BicZ<-function(X=X,Z=Z,Bic_null_vect=NULL,BicOld=NULL,methode=1,Zold=NULL){
+ if(is.null(Bic_null_vect)){
    val=calcul_BIC_mixmod(X=X)
-   Bic_vide_vect=val$BIC_vect
+   Bic_null_vect=val$BIC_vect
  }
  if(is.null(Zold)| is.null(BicOld)){
    Zold=0*Z
-   BicOld=Bic_vide_vect
+   BicOld=Bic_null_vect
  }
-    res=.Call( "BicZ",X,Z,Bic_vide_vect,BicOld,methode,Zold, PACKAGE = "CorReg")
+    res=.Call( "BicZ",X,Z,Bic_null_vect,BicOld,methode,Zold, PACKAGE = "CorReg")
     return(res$BIC)
 
 }
