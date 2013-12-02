1124094995073d7afd28371b0d837d3870d16694
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-02 08:09:29 +0000

    css
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -19,7 +19,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php echo $group_name; ?></title>
 	<link href="http://<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
-   <link href="http://<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+   <link href="style/estilo1.css" rel="stylesheet" type="text/css" />
   </head>
 
 <body>
