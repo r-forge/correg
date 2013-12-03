d0314222a03d296405882a44f677ced6e2aea0b3
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-03 14:43:11 +0000

    multiple try if wanted for search Z
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@62 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/rechercheZ.R
+++ pkg/CorReg/R/rechercheZ.R
@@ -15,9 +15,11 @@
 #'@param bla 0:pas de messages, 1:affiche le BIC,le numero d'etape et la complexite de Z quand il y'a un meilleur BIC, 2:affiche le BIC,le numero d'etape,la complexite de Z,le nombre de candidats et le BIC minimum observe parmi les candidats quand il y'a un meilleur BIC, 3: affiche en plus de bla=1 la complexite locale et le BIC local
 #'@param nb_opt_max stop criterion defining how many times the chain can stay at the max found
 #'@param exact boolean. If exact subregression is found it gives its content.
+#'@param nbini Number of initialisations (using Winitial). if NULL and Zini is NULL : only one chain beginning with zero matrix.
+#'@param ... parameters to be passed (for Winitial).
 #'@return etape 0:suppression,1 ajout,2 stationarite
 #'@export
-rechercheZ<-function(X=X,Z=NULL,Bic_null_vect=NULL,methode_tirage=-1,reject=0,methode_BIC=1,Rmax=5,p2max=NULL,Maxiter=1,plot=FALSE,best=TRUE,better=FALSE,random=TRUE,bla=1,nb_opt_max=NULL,exact=TRUE){
+rechercheZ<-function(X=X,Z=NULL,Bic_null_vect=NULL,methode_tirage=-1,reject=0,methode_BIC=1,Rmax=5,p2max=NULL,Maxiter=1,plot=FALSE,best=TRUE,better=FALSE,random=TRUE,bla=1,nb_opt_max=NULL,exact=TRUE,nbini=NULL,...){
   if(is.null(p2max)){
     p2max=ncol(X)+1 
   }
@@ -30,12 +32,28 @@ rechercheZ<-function(X=X,Z=NULL,Bic_null_vect=NULL,methode_tirage=-1,reject=0,me
   if(is.null(Bic_null_vect)){
      Bic_null_vect=calcul_BIC_mixmod(X=X,nbclustmax=10,bla=FALSE,details=FALSE,mclust=TRUE)$BIC_vect
   }
-  if(reject==0){#relax mode
-    res=.Call( "rechercheZ_relax",X,Z,Bic_null_vect,methode_tirage,methode_BIC,Rmax,p2max,Maxiter,plot,best,better,random,bla,nb_opt_max,exact, PACKAGE = "CorReg")
-    return(res)
-  }else{# reject mode
-    res=.Call( "rechercheZ_rejet",X,Z,Bic_null_vect,methode_tirage,methode_BIC,Rmax,p2max,Maxiter,plot,best,better,random,bla,nb_opt_max,exact, PACKAGE = "CorReg")
-    return(res)
+  if(is.null(nbini)){
+     if(reject==0){#relax mode
+        res=.Call( "rechercheZ_relax",X,Z,Bic_null_vect,methode_tirage,methode_BIC,Rmax,p2max,Maxiter,plot,best,better,random,bla,nb_opt_max,exact, PACKAGE = "CorReg")
+        return(res)
+     }else{# reject mode
+        res=.Call( "rechercheZ_rejet",X,Z,Bic_null_vect,methode_tirage,methode_BIC,Rmax,p2max,Maxiter,plot,best,better,random,bla,nb_opt_max,exact, PACKAGE = "CorReg")
+        return(res)
+     }
+  }else{
+     BICnull=sum(Bic_null_vect)
+     res=list()
+     for(i in 1:nbini){
+        if(reject==0){#relax mode
+           resloc=.Call( "rechercheZ_relax",X,Z,Bic_null_vect,methode_tirage,methode_BIC,Rmax,p2max,Maxiter,plot,best,better,random,bla,nb_opt_max,exact, PACKAGE = "CorReg")
+        }else{# reject mode
+           resloc=.Call( "rechercheZ_rejet",X,Z,Bic_null_vect,methode_tirage,methode_BIC,Rmax,p2max,Maxiter,plot,best,better,random,bla,nb_opt_max,exact, PACKAGE = "CorReg")
+        }
+        if(resloc$bic_opt<=min(res$bic_opt,BICnull)){
+           res=resloc
+        }
+     }
+     return(res)
   }
 
 }
