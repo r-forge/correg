41f8cf0cef2d7d9f2766c82e7d5fca1960f61a87
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-06 18:45:09 +0000

    new indicator to compare structures
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@75 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/CorReg/R/MSEZ.R
@@ -0,0 +1,20 @@
+#'@export
+MSEZ<-function(X=X,X_appr=NULL,B=NULL,Z=Z,scale=TRUE){
+   X=scale(X)
+   I1=which(colSums(Z)==0)
+   X1=cbind(1,X[,I1])
+   X2=X[,-I1]
+   I2=(1:ncol(Z))[-I1]
+   res=sum(apply(X1,2,var))
+   
+   if(is.null(B)){
+      if(is.null(X_appr)){X_appr=X
+      }else{
+         X_appr=scale(X_appr)
+      }
+      B=hatB(Z=Z,X=X_appr)
+   }
+   X2=X[,-colSums(Z)!=0]
+   res=res+sum(apply(X2-X1%*%B[-(I2+1),I2],2,var))
+   return(res)
+}
