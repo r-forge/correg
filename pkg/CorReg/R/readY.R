7397c9e27eb6a345b0cdadd391d469d2f88b13a2
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-10 09:41:26 +0000

    correct some warnings
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/readY.R
+++ pkg/CorReg/R/readY.R
@@ -1,3 +1,8 @@
+#' a summary-like function
+#' @param A coefficient vector
+#' @param labels name of the covariates
+#' @param X the dataset (named) if labels is null
+#' @param intercept boolean defining wether A contains an intercept or not
 #'@export
 readY<-function(A=A,labels=NULL,X=NULL,intercept=TRUE){
    if(is.null(labels)){
