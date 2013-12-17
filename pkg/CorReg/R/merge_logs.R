ae83a0e4552fbb2f0b959574a0b0dfeacdf98d39
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-17 16:23:06 +0000

    bug correction
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@94 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/merge_logs.R
+++ pkg/CorReg/R/merge_logs.R
@@ -1,15 +1,14 @@
 #' merge Rstudio Cran log files in a dataset
 #' @param logdirectory the path where files are placed
 #' @param packages vector of the names of the packages to keep in the global file(NULL will keep every packages)
-#' @param destfile the name of the created merged file
+#' @param destdir directory for export
 #' @export
-merge_logs<-function(logdirectory="CRANlogs",packages="CorReg",destfile=NULL){
+merge_logs<-function(logdirectory="CRANlogs",packages="CorReg",destdir=NULL){
    #later version would allow to choose a period
    #start=as.Date('2012-10-01'),today=format(Sys.Date(), "%Y-%m-%d")
-   file_list <- list.files(logdirectory, full.names=TRUE)
-   file_list=file_list[-length(file_list)]
+   file_list <- list.files(logdirectory, full.names=TRUE,pattern=".csv.gz")
    logs <- list()
-   for (file in file_list) {
+   for (file in file_list){
       print(paste("Reading", file, "..."))
       logs[[file]] <- read.table(file, header = TRUE, sep = ",", quote = "\"",
                                  dec = ".", fill = TRUE, comment.char = "", as.is=TRUE)
@@ -17,7 +16,6 @@ merge_logs<-function(logdirectory="CRANlogs",packages="CorReg",destfile=NULL){
    }
    
    # rbind together all files
-   require(data.table)
    dat <- rbindlist(logs)
    
    # add some keys and define variable types
@@ -28,13 +26,13 @@ merge_logs<-function(logdirectory="CRANlogs",packages="CorReg",destfile=NULL){
    dat[, week:=strftime(as.POSIXlt(date),format="%Y-%W")]
    
    setkey(dat, package, date, week, country)
-   if(is.null(destfile)){
-      save(dat, file=paste(logdirectory,"/CRANlogs",packages,".RData",sep=""))
+   if(is.null(destdir)){
+      save(data.frame(dat), file=paste(logdirectory,"/CRANlogs",paste(packages,collapse=""),".RData",sep=""))
       print(paste("load(",paste(logdirectory,"/CRANlogs",packages,".RData",sep=""),")"))
       
    }else{
-      save(dat, file=destfile)
-      print(paste("load(",destfile,")"))
+      filename=paste(destdir,"/CRANlogs",paste(packages,collapse=""),".csv",sep="")
+      write.csv(dat,file=filename)
    }
    return(dat)
 }
