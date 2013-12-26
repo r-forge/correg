e063e02b85e889c49d11bfcd72de836e143eca38
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-12-26 16:43:13 +0000

    new estimator (in test)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@151 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/correg.R
+++ pkg/CorReg/R/correg.R
@@ -22,11 +22,11 @@
 #' @param intercept boolean. If FALSE intercept will be set to 0 in each model.
 #' 
 correg<-function (X = X, Y = Y, Z = NULL, B = NULL, compl = TRUE, expl = TRUE, 
-                pred = TRUE, pred2=FALSE,
+                pred = TRUE, pred2=FALSE,prednew=FALSE,
                 select = "lar",
                 criterion = c("MSE", "BIC"),
                 X_test = NULL, Y_test = NULL, intercept = TRUE, 
-                K = 10, groupe = NULL, Amax = NULL, lambda = 1,retour=TRUE,final=FALSE) 
+                K = 10, groupe = NULL, Amax = NULL, lambda = 1,retour=TRUE,final=FALSE,nbalter=10,deltamin=0.01) 
 {
   res = list()
   X = as.matrix(X)
@@ -232,7 +232,113 @@ correg<-function (X = X, Y = Y, Z = NULL, B = NULL, compl = TRUE, expl = TRUE,
       }
       
     }
-    
+    if (prednew) {
+       if (is.null(B)) {
+          B = hatB(Z = Z, X = X)
+       }
+       nbit=0
+       deltaobs=deltamin+1
+       A_old=rep(0,times=(ncol(Z)+intercept))
+       while(nbit<nbalter & deltaobs>deltamin){
+          nbit=nbit+1
+          #on fait tout comme d'hab
+             Xtilde = X[, I2] - cbind(rep(1, times = nrow(X)), X[, I1]) %*% B[c(1, I1 + 1), I2]
+             Xtilde = as.matrix(Xtilde)
+             if(intercept){
+                Ytilde = Y - as.matrix(X[, I1]) %*% A_expl[-1][I1] - A_expl[1]
+             }else{
+                Ytilde = Y - as.matrix(X[, I1]) %*% A_expl[I1]
+             }
+             if (select == "NULL") {
+                A_inj = OLS(X = Xtilde, Y = Ytilde, intercept = F)$beta
+             }else if (select != "elasticnet"  & select != "ridge") {
+                lars_inj = lars(x = Xtilde, y = Ytilde, type = select, 
+                                intercept = F)
+                A_inj = meilleur_lars(lars = lars_inj, X = Xtilde, 
+                                      Y = Ytilde, mode = criterion, intercept = F, 
+                                      K = K, groupe = groupe)$A
+             }else if (select=="elasticnet") {
+                lars_inj = renet(x = Xtilde, y = Ytilde, intercept = F, 
+                                 lambda = lambda)
+                names(lars_inj)[4] = "coefficients"
+                A_inj = meilleur_lars(lars = lars_inj, X = Xtilde, 
+                                      Y = Ytilde, mode = criterion, intercept = F, 
+                                      K = K, groupe = groupe)$A
+             }else{#ridge
+                ridge_pred = linearRidge(Ytilde~0+.,data=data.frame(Xtilde))
+                A_inj=coef(ridge_pred)
+             }
+             A_pred = rep(0, times = ncol(X) + intercept)
+             A_pred[I2 + intercept] = A_inj
+             if(retour){
+                Ytildebis=Y-as.matrix(X[,I2])%*%A_pred[I2 + intercept]
+                Ytildebis=as.matrix(Ytildebis)
+                if (select != "elasticnet" & select != "ridge") {
+                   lars_retour=lars(x = as.matrix(X[,I1]), y = Ytildebis, type = select, 
+                                    intercept = intercept)
+                   A_retour = meilleur_lars(lars = lars_retour, X = as.matrix(X[,I1]), 
+                                            Y = Ytildebis, mode = criterion, intercept = intercept, 
+                                            K = K, groupe = groupe)$A
+                }else if (select=="elasticnet"){
+                   lars_retour= renet(x = as.matrix(X[,I1]), y = Ytildebis, intercept =intercept, 
+                                      lambda = lambda)
+                   names(lars_retour)[4] = "coefficients"
+                   A_retour = meilleur_lars(lars = lars_retour, X = as.matrix(X[,I1]), 
+                                            Y = Ytildebis, mode = criterion, intercept = intercept, 
+                                            K = K, groupe = groupe)$A
+                }else{#ridge
+                   ridge_pred = linearRidge(Ytildebis~.,data=data.frame(X[,I1]))
+                   A_retour=coef(ridge_pred)
+                }
+                
+                if(intercept){
+                   A_pred[c(intercept, I1 + intercept)] =A_retour
+                }else{
+                   A_pred[ I1 ] = A_retour
+                }
+             }else{
+                if(intercept){
+                   A_pred[c(intercept, I1 + intercept)] = A_expl[c(intercept,I1 + intercept)] 
+                   - B[c(1, I1 + 1), I2] %*% as.matrix(A_inj)
+                }else{
+                   A_pred[ I1 ] = A_expl[I1] 
+                   - B[c(1, I1 + 1), I2] %*% as.matrix(A_inj)
+                }
+                
+             }
+             
+             res$prednew$A = A_pred
+             res$prednew$CVMSE = CVMSE(X = X[, which(A_pred[-1] != 
+                                                     0)], Y = Y, intercept = intercept, K = K, groupe = groupe)
+             res$prednew$BIC = BicTheta(X = X, Y = Y, intercept = intercept, 
+                                     beta = A_pred)
+             if(final){
+                if(intercept){
+                   quifinal=which(A_pred[-1]!=0)
+                }else{
+                   quifinal=which(A_pred!=0)
+                }
+                Zfinal=as.matrix(Z[quifinal,quifinal])
+                A_final=correg(X=X[,quifinal],Y=Y,Z=Zfinal,B=as.matrix(B[c(1,quifinal+1),quifinal]),retour=F,final=F,groupe=groupe,K=K,intercept=intercept,criterion=criterion,select="NULL")$pred$A
+                
+                res$finalnew$A=res$prednew$A
+                res$finalnew$A[res$prednew$A!=0]=A_final
+                res$finalnew$CVMSE = CVMSE(X = as.matrix(X[,quifinal]), Y = Y, intercept = intercept, K = K, groupe = groupe)
+                res$finalnew$BIC = BicTheta(X = as.matrix(X[,quifinal]), Y = Y, intercept = intercept, 
+                                         beta = A_final)
+             }
+          #une fois A calculé, on regarde si on doit continuer
+          deltaobs=sum(abs(A_old-res$prednew$A))
+          if(deltaobs>deltamin & nbit<nbalter){#on continue
+            #on estime un nouveau B
+             print(deltaobs)
+             A_old=res$prednew$A
+            B=newhatB(X=X,Z=Z,A=res$prednew$A,Atilde=res$expl$A,Bold=B,intercept=intercept)
+          
+          }
+          
+       } #fin de la boucle d'optimisation alternée     
+    }
   }else if (sum(Z) == 0 & (expl | pred)) {
     res$expl$A = res$compl$A
     if (pred) {
