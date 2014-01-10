a8aaab25e1c8d034143cf19f5c58d258a455a74a
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2014-01-10 16:12:49 +0000

    makeF
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@199 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/CorReg/R/MakeF.R
@@ -0,0 +1,27 @@
+# Atilde de taille p1
+MakeF<-function(X=X,Z=Z,B=B,Sigma=Sigma,A=A,lambda=lambda,Atilde=Atilde){
+   
+   X=cbind(1,X)
+   I2=which(colSums(Z)!=0)
+   p2=length(I2)
+   p1=ncol(X)-p2#prendre donc en compte la constante
+   Z=rbind(0,Z)
+   Z[1,I2]=1#on ajoute une constante à chaque ssreg
+   Z=cbind(0,Z)
+   I2=I2+1
+   pz=sum(Z!=0)
+   n=nrow(X)
+   Fvect=rep(0,times=(p1+p2+pz))
+   barZ=which(Z!=0,arr.ind=T)
+   lambda
+   for(j in 1:p2){
+      I1j=barZ[barZ[,2]==I2[j],1]
+      debcolj=nrow(barZ[barZ[,2]<I2[j],])
+      colonne=(debcolj+1):(debcolj+sum(Z[,I2[j]])) #sous-reg precedentes+
+      Fvect[colonne]=(1/Sigma[j]^2)*t(X[,I1j])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])+A[I2[j]+1]*lambda[which(Z[-I2,I2[j]]!=0)]     
+      
+      
+   }  
+   Fvect[(pz+1):pz+p1]=A[-I2]+B[-I2,I2]%*%A[I2]-Atilde
+   return(Fvect)
+}
