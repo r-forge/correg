5284033730c9724500af5ca274c0d77d62f0e236
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2014-01-13 09:16:37 +0000

    nw step function for newton
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@201 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/CorReg/R/newtheta.R
@@ -0,0 +1,38 @@
+newtheta<-function(X=X,Z=Z,B=B,Sigma=Sigma,A=A,lambda=lambda,Atilde=Atilde,nbit=1){
+   
+   X=cbind(1,X)
+   I2=which(colSums(Z)!=0)
+   p2=length(I2)
+   p1=ncol(X)-p2#prendre donc en compte la constante
+   Z=rbind(0,Z)
+   Z[1,I2]=1#on ajoute une constante à chaque ssreg
+   Z=cbind(0,Z)
+   I2=I2+1
+   pz=sum(Z!=0)
+   n=nrow(X)
+   J=matrix(0,ncol=(p1+p2+pz),nrow=(p1+p2+pz))
+   barZ=which(Z!=0,arr.ind=T)   
+   Fvect=rep(0,times=(p1+p2+pz))
+   if(is.null(lambda)){
+      lambda=rep(1,times=p1)
+   }
+   for(j in 1:p2){
+      I1j=barZ[barZ[,2]==I2[j],1]
+      debcolj=nrow(barZ[barZ[,2]<I2[j],])
+      colonne=(debcolj+1):(debcolj+sum(Z[,I2[j]])) #sous-reg precedentes+
+      Fvect[colonne]=(1/Sigma[j]^2)*t(X[,I1j])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])+A[I2[j]+1]*lambda[which(Z[-I2,I2[j]]!=0)]     
+      Fvect[pz+p1+j]=Sigma[j]^2-(1/n)*t(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])
+      
+      J[pz+p1+j,pz+p1+j]=2*Sigma[j]#bloc J9
+      J[pz+p1+j,colonne]=(2/n)*t(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])%*%X[,I1j]#bloc J7
+      J[pz+I1j,which(barZ[,2]==I2[j])]=A[I2[j]+1]#attention on compte l'intercept #blocJ4
+      J[colonne,pz+p1+j]=(-2/(Sigma[j]^3))*t(X[,I1j])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]]) #bloc J3 
+      J[which(barZ[,2]==I2[j]),pz+I1j]=A[I2[j]+1]#attention on compte l'intercept #bloc J2
+      diag(J[I1j,I1j])=(-1/(Sigma[j]^2))*diag(t(X[,I1j])%*%(X[,I1j]))#bloc J1
+   }  
+   Fvect[(pz+1):(pz+p1)]=A[-I2]+B[-I2,I2]%*%A[I2]-Atilde
+   
+   matint=solve(J)%*%Fvect
+   
+   return(list(B=B,Sigma=Sigma,lambda=lambda))
+}
