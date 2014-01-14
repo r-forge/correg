3a7a84bdfaaa1a7b3ab8ba108bbbd9ccab62a97e
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-14 18:15:09 +0000

    u
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@215 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/newtheta.R
+++ pkg/CorReg/R/newtheta.R
@@ -1,5 +1,8 @@
-newtheta<-function(X=X,Z=Z,B=B,Sigma=Sigma,A=A,lambda=lambda,Atilde=Atilde,nbit=1){
-   
+#' @param X dataset without intercept
+#' @param Z the structure (square without intercept)
+#' @param B the wheighted structure
+#' @export
+newtheta<-function(X=X,Z=Z,B=B,Sigma=Sigma,A=A,lambda=NULL,Atilde=Atilde,nbit=1){
    X=cbind(1,X)
    I2=which(colSums(Z)!=0)
    p2=length(I2)
@@ -12,27 +15,35 @@ newtheta<-function(X=X,Z=Z,B=B,Sigma=Sigma,A=A,lambda=lambda,Atilde=Atilde,nbit=
    n=nrow(X)
    J=matrix(0,ncol=(p1+p2+pz),nrow=(p1+p2+pz))
    barZ=which(Z!=0,arr.ind=T)   
-   Fvect=rep(0,times=(p1+p2+pz))
+   Fvect=rep(0,times=(p1+p2+pz))   
    if(is.null(lambda)){
       lambda=rep(1,times=p1)
    }
-   for(j in 1:p2){
-      I1j=barZ[barZ[,2]==I2[j],1]
-      debcolj=nrow(barZ[barZ[,2]<I2[j],])
-      colonne=(debcolj+1):(debcolj+sum(Z[,I2[j]])) #sous-reg precedentes+
-      Fvect[colonne]=(1/Sigma[j]^2)*t(X[,I1j])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])+A[I2[j]+1]*lambda[which(Z[-I2,I2[j]]!=0)]     
-      Fvect[pz+p1+j]=Sigma[j]^2-(1/n)*t(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])
-      
-      J[pz+p1+j,pz+p1+j]=2*Sigma[j]#bloc J9
-      J[pz+p1+j,colonne]=(2/n)*t(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])%*%X[,I1j]#bloc J7
-      J[pz+I1j,which(barZ[,2]==I2[j])]=A[I2[j]+1]#attention on compte l'intercept #blocJ4
-      J[colonne,pz+p1+j]=(-2/(Sigma[j]^3))*t(X[,I1j])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]]) #bloc J3 
-      J[which(barZ[,2]==I2[j]),pz+I1j]=A[I2[j]+1]#attention on compte l'intercept #bloc J2
-      diag(J[I1j,I1j])=(-1/(Sigma[j]^2))*diag(t(X[,I1j])%*%(X[,I1j]))#bloc J1
+   for (it in 1:nbit){       
+      for(j in 1:p2){
+         I1j=barZ[barZ[,2]==I2[j],1]
+         debcolj=nrow(barZ[barZ[,2]<I2[j],])
+         colonne=(debcolj+1):(debcolj+sum(Z[,I2[j]])) #sous-reg precedentes+
+         Fvect[colonne]=(1/Sigma[j]^2)*t(X[,I1j])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])+A[I2[j]+1]*lambda[which(Z[-I2,I2[j]]!=0)]     
+         Fvect[pz+p1+j]=Sigma[j]^2-(1/n)*t(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])
+         
+         J[pz+p1+j,pz+p1+j]=2*Sigma[j]#bloc J9
+         J[pz+p1+j,colonne]=(2/n)*t(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]])%*%X[,I1j]#bloc J7
+         diag(J[pz+(1:p1),which(barZ[,2]==I2[j])])=A[I2[j]+1]#attention on compte l'intercept #blocJ4
+         J[colonne,pz+p1+j]=(-2/(Sigma[j]^3))*t(X[,I1j])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]]) #bloc J3 
+         diag(J[which(barZ[,2]==I2[j]),pz+(1:p1)])=A[I2[j]+1]#attention on compte l'intercept #bloc J2
+         diag(J[colonne,colonne])=(-1/(Sigma[j]^2))*diag(t(X[,I1j])%*%(X[,I1j]))#bloc J1
+      }  
+      Fvect[(pz+1):(pz+p1)]=A[-I2]+B[-I2,I2]%*%A[I2]-Atilde   
+      matint=solve(J)%*%Fvect
+      for (j in 1:p2){
+         I1j=barZ[barZ[,2]==I2[j],1]
+         debcolj=nrow(barZ[barZ[,2]<I2[j],])
+         colonne=(debcolj+1):(debcolj+sum(Z[,I2[j]])) #sous-reg precedentes+
+         B[I1j,I2[j]]=matint[colonne]
+      }
+      Sigma=matint[-c(1:(pz+p1))]
+      lambda=matint[c((pz+1):(pz+p1))]
    }  
-   Fvect[(pz+1):(pz+p1)]=A[-I2]+B[-I2,I2]%*%A[I2]-Atilde
-   
-   matint=solve(J)%*%Fvect
-   
    return(list(B=B,Sigma=Sigma,lambda=lambda))
 }
