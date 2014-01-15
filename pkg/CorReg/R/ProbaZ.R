f256b618373fa707c4267408348b745529582313
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2014-01-15 11:17:55 +0000

    Bic*
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@217 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/ProbaZ.R
+++ pkg/CorReg/R/ProbaZ.R
@@ -1,28 +1,44 @@
 #' Probability of Z without knowing the dataset. It also gives the exact number of binary nilpotent matrices of size p.
 #' @param p the number of covariates
 #' @param Z the structure
+#' @param star gives the log pra under uniform law for p2
+#' @param proba gives the proba under the uniform law for Z
 #' @export
 #' 
 #'
-ProbaZ<-function(p=NULL,Z=NULL,proba=FALSE){
-   if(is.null(p)){
-      if(!is.null(Z)){
-         p=ncol(Z)
-         
-      }else{
-         print("missing parameters")
+ProbaZ<-function(Z=NULL,p=NULL,proba=FALSE,star=TRUE){
+   if(star & !is.null(Z)){
+      p=ncol(Z)
+      p1j=colSums(Z)
+      I2=which(p1j!=0)
+      p1j=p1j[I2]
+      p2=length(I2)
+      logproba=0
+      logproba=logproba-log(p2)-p2*log(p-p2)-log(choose(p,p2))
+      for (j in 1:p2){
+         logproba=logproba-log(choose((p-p2),p1j[j]))
       }
-   }
-   nb=1#modele vide
-   if(p>1){
-      #calcul du nombre de modèles
-      for (i in 1:(p-1)){
-         nb=nb+choose(p,i)*(2^(p-i)-1)^i
-      }
-   }
-   if(proba){
-      return(1/nb)
+      return(logproba)
    }else{
-      return(nb)
+      if(is.null(p)){
+         if(!is.null(Z)){
+            p=ncol(Z)
+            
+         }else{
+            print("missing parameters")
+         }
+      }
+      nb=1#modele vide
+      if(p>1){
+         #calcul du nombre de modèles
+         for (i in 1:(p-1)){
+            nb=nb+choose(p,i)*(2^(p-i)-1)^i
+         }
+      }
+      if(proba){
+         return(1/nb)
+      }else{
+         return(nb)
+      } 
    }
 }
