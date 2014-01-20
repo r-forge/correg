554d153a6526ad5bb205b7582c14715b51cb34e0
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-01-20 09:16:37 +0000

    bug on lambda
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@233 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/newtheta.R
+++ pkg/CorReg/R/newtheta.R
@@ -12,6 +12,7 @@ newtheta<-function(X=X,Z=Z,B=B,Sigma=Sigma,A=A,lambda=NULL,Atilde=Atilde,nbit=1)
    Z=cbind(0,Z)
    I2=I2+1
    pz=sum(Z!=0)
+   quidroite=which(rowSums(Z[,])!=0)
    p1=length(which(rowSums(Z[-I2,])!=0))#I3 ne doit pas intervenir
    if(is.null(lambda)){
       lambda=rep(1,times=p1)
@@ -26,7 +27,7 @@ newtheta<-function(X=X,Z=Z,B=B,Sigma=Sigma,A=A,lambda=NULL,Atilde=Atilde,nbit=1)
          I1j=barZ[barZ[,2]==I2[j],1]
          debcolj=nrow(barZ[barZ[,2]<I2[j],])
          colonne=(debcolj+1):(debcolj+sum(Z[,I2[j]])) #sous-reg precedentes+
-         Fvect[colonne]=(1/Sigma[j]^2)*t(X[,I1j])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]-1])+A[I2[j]]*lambda[which(Z[-I2,I2[j]]!=0)]     
+         Fvect[colonne]=(1/Sigma[j]^2)*t(X[,I1j])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]-1])+A[I2[j]]*lambda[which(Z[quidroite,I2[j]]!=0)]     
          Fvect[pz+p1+j]=Sigma[j]^2-(1/n)*t(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]-1])%*%(X[,I2[j]]-X[,I1j]%*%B[I1j,I2[j]-1])
          
          J[pz+p1+j,pz+p1+j]=2*Sigma[j]#bloc J9
@@ -36,7 +37,7 @@ newtheta<-function(X=X,Z=Z,B=B,Sigma=Sigma,A=A,lambda=NULL,Atilde=Atilde,nbit=1)
          diag(J[which(barZ[,2]==I2[j]),pz+(1:p1)])=A[I2[j]]#attention on compte l'intercept #bloc J2
          diag(J[colonne,colonne])=(-1/(Sigma[j]^2))*diag(t(X[,I1j])%*%(X[,I1j]))#bloc J1
       }  
-      Fvect[(pz+1):(pz+p1)]=A[-I2]+B[-I2,I2-1]%*%A[I2]-Atilde   
+      Fvect[(pz+1):(pz+p1)]=A[-I2]+as.matrix(B[-I2,I2-1])%*%A[I2]-Atilde   
       if(rcond(J)>10^(-16)){
          matint=solve(J)%*%Fvect
          for (j in 1:p2){
