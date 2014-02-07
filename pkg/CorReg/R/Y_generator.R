cbb89af163a141a3ac323f222e933d1dc46e477c
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-07 17:45:13 +0000

    test rforge
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@376 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/Y_generator.R
+++ pkg/CorReg/R/Y_generator.R
@@ -2,12 +2,12 @@
 #' @param X the dataset
 #' @param Amax maximum number of non-zero coefficients
 #' @param sigma_Y the standard deviation of the noise
-#' @param positif the ratio of positive coefficients
+#' @param positive the ratio of positive coefficients
 #'@export 
-Y_generator<-function(X=X,Amax=NULL,sigma_Y=10,positif=0.6){
+Y_generator<-function(X=X,Amax=NULL,sigma_Y=10,positive=0.6){
   p=ncol(X)
   taille=nrow(X)
-  A=rpois(p+1,5)*(rep(-1,p+1)+2*rbinom(p+1,1,positif)) 
+  A=rpois(p+1,5)*(rep(-1,p+1)+2*rbinom(p+1,1,positive)) 
   if(!is.null(Amax)){
     Amax=min(abs(Amax),p)
     A[-sample(2:(p+1),size=Amax)]=0#on garde la constante
