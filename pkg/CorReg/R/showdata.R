c56e2250170901bced8bde44bebeb97a788d5f60
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2014-02-27 08:22:31 +0000

    change naming and add new functionnality
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@402 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/CorReg/R/showdata.R
@@ -0,0 +1,22 @@
+#' show the missing values of a dataset
+#'
+#'@param X the dataset to analyse
+#'@param what indicates what to plot
+#'@export
+
+showdata<-function(X=W,what=c("miss","correl")){
+   what=what[1]
+   if(what=="miss"){
+      M=which(is.na(X),arr.ind=T)
+      if(nrow(M)>1){
+         plot(M[,c(2,1)],pch=7)
+         title("Missing values in the dataset")  
+      }else{
+         print("No missing values")
+      }
+   }else{
+      correl=cor(X[,!is.na(colSums(X)) & apply(base_num,2,sd)!=0])
+      corrplot(corr=correl,addrect=NULL,is.corr=T,method="color",tl.pos="n",diag=F,outline=F)
+   }
+
+}
