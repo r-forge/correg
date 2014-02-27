64b4a502546355ad82f9a177c5d801db3ecd87a9
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-02-27 08:23:00 +0000

    recursive tree to dodge correlations
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@403 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/CorReg/R/recursive_tree.R
@@ -0,0 +1,32 @@
+#' decision tree in a recursive way
+#'@export
+#'@param data the dataset including the response
+#'@param Y the name of the response
+#'@param modele vector of the names used for the last tree search (not necessarily choosen)
+#'@param kill vector of the names to kill
+#'@param index to give a number to the plot
+#'@param print boolean to print the tree parameters
+#'@param plot boolean to plot the tree
+#'
+recursive_tree<-function(data=data,Y="Y",modele=NULL,kill=NULL,index=NULL,print=TRUE,plot=TRUE){
+   if (is.null(modele)){
+      modele=colnames(data)
+      modele=modele[modele!=Y]   
+   }
+   if(!is.null(kill)){
+      modele=modele[!modele%in%kill]
+   }
+   
+   formule=as.formula(paste(Y," ~",paste(modele,collapse="+")))
+   arbre=rpart(formule,data);par( xpd=NA)
+   if(plot){
+      sub="oui à gauche, non à droite. Moyenne et effectif"
+      ylab="la hauteur indique la significativité"
+      plot(arbre,sub=sub);text(arbre, use.n=TRUE)
+      #title(main = titre,ylab=vertical,xlab = paste(infos,"\n Oui Ã  gauche, non Ã  droite.",reponse), col.main = "red", col.lab = gray(.5),cex.main = 1.2, cex.lab = 1.0, font.main = 4, font.lab = 3
+   }
+   if(print){
+      print(arbre)
+   }
+   return(list(tree=arbre,modele=modele))
+}
