271f940ee596f29b365a329d18685172a19574de
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2014-02-28 10:36:36 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@414 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/Y_generator.R
+++ pkg/CorReg/R/Y_generator.R
@@ -4,14 +4,18 @@
 #' @param sigma_Y the standard deviation of the noise
 #' @param positive the ratio of positive coefficients
 #'@export 
-Y_generator<-function(X=X,Amax=NULL,sigma_Y=10,positive=0.6){
+Y_generator<-function(X=X,Amax=NULL,sigma_Y=10,positive=0.6,Z=NULL,tp1=1,tp2=2,tp3=3){
   p=ncol(X)
   taille=nrow(X)
-  A=rpois(p+1,5)*(rep(-1,p+1)+2*rbinom(p+1,1,positive)) 
-  if(!is.null(Amax)){
-    Amax=min(abs(Amax),p)
-    A[-sample(2:(p+1),size=Amax)]=0#on garde la constante
+  A=rpois(p+1,5)*(rep(-1,p+1)+2*rbinom(p+1,1,positive))
+  if(is.null(Z)){
+     Z=matrix(0,ncol=ncol(X),nrow=nrow(X))
   }
+  A=generateurA_ou(Z=Z,tp1=tp1,tp2=tp2,tp3=tp3,positive=positive,pb=pb,Amax=Amax)
+#   if(!is.null(Amax)){
+#     Amax=min(abs(Amax),p)
+#     A[-sample(2:(p+1),size=Amax)]=0#on garde la constante
+#   }
   Y=cbind(rep(1,times=taille),as.matrix(X))%*%A+rnorm(taille,mean=0,sd=sigma_Y)
   return(list(Y=Y,A=A))
 }
