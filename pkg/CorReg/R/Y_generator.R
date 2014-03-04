f10e6e43132fc373d224e89a3422707f95239e9e
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-04 12:58:17 +0000

    some new parameters
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@416 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/Y_generator.R
+++ pkg/CorReg/R/Y_generator.R
@@ -3,11 +3,16 @@
 #' @param Amax maximum number of non-zero coefficients
 #' @param sigma_Y the standard deviation of the noise
 #' @param positive the ratio of positive coefficients
+#' @param tp1 ratio of the right-side covariates in A
+#' @param tp2 ratio of the left-side covariates in A
+#' @param tp3 ratio of the totally independent covariates (if exists) in A
+#' @param pb kind of problem : 0=none, 1=simple, 2=strong
+#' @param Z the structure adjacency matrix
 #'@export 
-Y_generator<-function(X=X,Amax=NULL,sigma_Y=10,positive=0.6,Z=NULL,tp1=1,tp2=2,tp3=3){
+Y_generator<-function(X=X,Amax=NULL,sigma_Y=10,positive=0.6,Z=NULL,tp1=1,tp2=2,tp3=3,pb=2){
   p=ncol(X)
   taille=nrow(X)
-  A=rpois(p+1,5)*(rep(-1,p+1)+2*rbinom(p+1,1,positive))
+#   A=rpois(p+1,5)*(rep(-1,p+1)+2*rbinom(p+1,1,positive))
   if(is.null(Z)){
      Z=matrix(0,ncol=ncol(X),nrow=nrow(X))
   }
