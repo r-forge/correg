b6c990aafd1b8e6aa370d968124db4eed16915d1
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-04 14:30:11 +0000

    bug removal
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@417 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/readY.R
+++ pkg/CorReg/R/readY.R
@@ -13,9 +13,11 @@ readY<-function(A=A,labels=NULL,X=NULL,intercept=TRUE,ANOVA=FALSE,print=TRUE,Y=N
    }
    if(intercept){labels=c("intercept",labels)}
    interp=cbind(A[A!=0],labels[A!=0])
-   Xred=X[,A[-intercept]!=0]
-   reslm=lm(Y~.,data=data.frame(Xred))
-   interp=summary(reslm)  
+   if(ANOVA & !is.null(X) & !is.null(Y)){
+      Xred=X[,A[-intercept]!=0]
+      reslm=lm(Y~.,data=data.frame(Xred))
+      interp=summary(reslm) 
+   }
    if(print){print(interp)}
    return(interp)
 }
