fbd35adb628e3919c55f5083ee22356cd1795626
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-12 14:21:07 +0000

    test0 bug clang
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@430 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/src/BICZsparse_cpp.cpp
+++ pkg/CorReg/src/BICZsparse_cpp.cpp
@@ -8,6 +8,7 @@ using namespace Eigen;
 using namespace std;
 using Eigen::Map;
 using Eigen::MatrixXd;
+using Eigen::VectorXd;
 using Rcpp::as;
 
 Eigen::VectorXd BICZsparse_cpp(Eigen::MatrixXd X,NumericVector Z_Zi,Eigen::VectorXd Z_Sj,Eigen::VectorXd Bic_vide_vect,Eigen::VectorXd BicOld,int methode,Eigen::VectorXd Zold_Sj,int nrow)
--- pkg/CorReg/src/BICZsparse_cpp.h
+++ pkg/CorReg/src/BICZsparse_cpp.h
@@ -3,6 +3,12 @@
 
 #include <RcppEigen.h>
 using namespace Rcpp ;
+using namespace Eigen;
+using namespace std;
+using Eigen::Map;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using Rcpp::as;
 RcppExport Eigen::VectorXd BICZsparse_cpp(Eigen::MatrixXd X,NumericVector Z_Zi,Eigen::VectorXd Z_Sj,Eigen::VectorXd Bic_vide_vect,Eigen::VectorXd BicOld,int methode,Eigen::VectorXd Zold_Sj,int nrow) ;
 
 #endif
