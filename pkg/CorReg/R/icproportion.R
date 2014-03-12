d264bc795739cc8f6ffd40f985e26ca02f0cd059
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2014-03-12 17:49:49 +0000

    wilson method for proportions
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@435 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/icproportion.R
+++ pkg/CorReg/R/icproportion.R
@@ -1,6 +1,9 @@
 icproportion<-function(prop=prop, n=n, alpha=0.05){
    inter=alpha/2#seuil de confiance
-   lower =prop + qnorm(inter) * sqrt(prop * (1 -prop)/n)
-   upper =prop + qnorm(1-inter) * sqrt(prop * (1 -prop)/n)
+   z=qnorm(1-inter)
+   basic=(2*n*prop+z^2)/(2*(n+z^2))
+   complement=(z*sqrt(z^2+4*n*prop*(1-prop)))/(2*(n+z^2))
+   lower =basic-complement
+   upper =prop + complement
    return(c(lower, upper))
 }
