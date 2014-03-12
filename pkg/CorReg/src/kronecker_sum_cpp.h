270ec4b2f7b41408b75f5c760bafaf23432914bb
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-12 13:26:28 +0000

    bug clang
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@429 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/src/kronecker_sum_cpp.h
+++ pkg/CorReg/src/kronecker_sum_cpp.h
@@ -2,6 +2,7 @@
 #define _CorReg_kronecker_sum_H
 
 
+
 #include <RcppEigen.h>
 using namespace Rcpp ;
 RcppExport Rcpp::NumericVector kronecker_sum_cpp(const Rcpp::NumericVector &V1,const Rcpp::NumericVector &V2);
