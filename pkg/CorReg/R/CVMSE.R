83319bf9cfad0c25e5e3ce05513b7d885a081625
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2014-03-12 06:29:58 +0000

    update
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@425 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/CVMSE.R
+++ pkg/CorReg/R/CVMSE.R
@@ -1,10 +1,10 @@
-# ' Cross validation
-# ' 
-# ' @param X covariates matrix (double)
-# ' @param Y response variable
-# ' @param K number of classes
-# ' @param intercept (boolean) with or without an intercept
-# ' @param methode the methode used by OLS.
+#' Cross validation
+#' @export
+#' @param X covariates matrix (double)
+#' @param Y response variable
+#' @param K number of classes
+#' @param intercept (boolean) with or without an intercept
+#' @param methode the methode used by OLS.
 CVMSE<-function(X=X,Y=Y,K=K,intercept=T,methode=1,groupe=NULL){
   K=abs(K)
   K=min(K,nrow(X))
