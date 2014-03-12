9b2971df52b1dbe5b10a7425819125874010d412
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2014-03-12 13:26:28 +0000

    bug clang
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@429 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/src/kronecker_prod_cpp.h
+++ pkg/CorReg/src/kronecker_prod_cpp.h
@@ -2,6 +2,7 @@
 #define _CorReg_kronecker_prod_H
 
 
+
 #include <RcppEigen.h>
 using namespace Rcpp ;
 RcppExport Rcpp::NumericVector kronecker_prod_cpp(const Rcpp::NumericVector &V1,const Rcpp::NumericVector &V2);
