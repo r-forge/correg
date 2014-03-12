e369da9693fc38f413602d08bd7b5d18f68fa8aa
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-03-12 15:13:17 +0000

    last clang bug ?
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@433 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/src/BICZsparse_j_cpp.h
+++ pkg/CorReg/src/BICZsparse_j_cpp.h
@@ -3,6 +3,6 @@
 
 #include <RcppEigen.h>
 using namespace Rcpp ;
-RcppExport Eigen::VectorXd BICZsparse_j_cpp(Eigen::MatrixXd X,Eigen::VectorXd Z_Zi,Eigen::VectorXd Z_Sj,Eigen::VectorXd Bic_vide_vect,Eigen::VectorXd BicOld,int methode,int nrow,int j) ;
+Eigen::VectorXd BICZsparse_j_cpp(Eigen::MatrixXd X,Eigen::VectorXd Z_Zi,Eigen::VectorXd Z_Sj,Eigen::VectorXd Bic_vide_vect,Eigen::VectorXd BicOld,int methode,int nrow,int j) ;
 
 #endif
