f159a8fdeb5bbfcf13d3330b3545ecffb3eaa4bf
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-05-15 07:25:08 +0000

    up
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@601 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/CorReg-package.R
+++ pkg/CorReg/R/CorReg-package.R
@@ -28,7 +28,7 @@
 #'    
 #'    #MCMC to find the structure
 #'    res=searchZ(X=X_appr,verbose=0,reject=0,Maxiter=900,
-#'                nbtry=30,candidates=-1,Bic_null_vect=BIC_empty_vect,star=TRUE,p1max=15,clean=TRUE)
+#'                nbini=30,candidates=-1,Bic_null_vect=BIC_empty_vect,star=TRUE,p1max=15,clean=TRUE)
 #'    hatZ=res$Z_opt
 #'    hatBic=res$bic_opt
 #'    
