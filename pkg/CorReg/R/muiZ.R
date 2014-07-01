a34a26d22391041bae29f3d3c564c319672a2921
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-07-01 08:25:05 +0000

    up
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@672 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/muiZ.R
+++ pkg/CorReg/R/muiZ.R
@@ -1,4 +1,4 @@
-
+# ' computes the parameter of the joint law X for a given individual (and its components)
 # ' @param alpha the matrix of coefficients of the sub-regressions
 muiZ<-function(mixmod=mixmod,components=components,Z=Z,Zc=Zc,alpha=alpha,Ir=Ir,sigma_IR=sigma_IR){ 
    mui=components#en C on les prendra en entrée pour gagner une initialisation à chaque fois
@@ -24,8 +24,8 @@ muiZ<-function(mixmod=mixmod,components=components,Z=Z,Zc=Zc,alpha=alpha,Ir=Ir,s
             Sigma[i,j]=alpha[-1,][i,j]*sigmai[i]
             Sigma[j,i]=Sigma[i,j]
          }
-      }
-      
+      } 
    }
-   return(list(mui=mui,sigmai=sigmai,Sigma=Sigma))
+   return(list(mui=mui,sigmai=sigmai,Sigma=Sigma))#en fait sigmai ne sert à rien car stocké dans la diagonale de Sigma
 }
+#version C: laisser comp_vect pour éviter les recopie.
