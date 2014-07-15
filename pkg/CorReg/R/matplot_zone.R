ce04bbd9471e62db5a60051009e7bc61ebe28a43
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2014-07-15 13:00:55 +0000

    up bender
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@688 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/matplot_zone.R
+++ pkg/CorReg/R/matplot_zone.R
@@ -1,3 +1,4 @@
+#' draws matplot with conditionnal background for easier comparison of curves.
 #' @export
 matplot_zone<-function(x=x,y=y,col=1:6,alpha=0.2,what=which.min,ylim=NULL,xlim=NULL,type="p",xlab=NULL,ylab="NULL",main=NULL,lwd=lwd){
    matplot(x,y,ylim=ylim,type=type,xlab=xlab,ylab=ylab,main=main,lwd=lwd,xlim=xlim)
