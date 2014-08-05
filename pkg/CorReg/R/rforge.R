274cb48c36282d61ee1d8443528f615d64d91ddf
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2014-08-05 08:36:00 +0000

    up rforge
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@753 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/rforge.R
+++ pkg/CorReg/R/rforge.R
@@ -1,5 +1,5 @@
-#' to upgrade a package to the lastest version on R-forge
-#' @param package vector of the name of the packages to upgrade
+#' Upgrades a package to the lastest version on R-forge
+#' @param packages vector of the name of the packages to upgrade
 #' @export
 rforge<-function(packages=c("CorReg"),repos="http://R-Forge.R-project.org"){
    install.packages(packages, repos=repos)
