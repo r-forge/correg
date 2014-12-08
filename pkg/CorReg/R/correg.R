242e198f915d9fced51aea23f3255cbf835124c9
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-12-08 12:59:05 +0000

    up
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@969 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/correg.R
+++ pkg/CorReg/R/correg.R
@@ -35,20 +35,16 @@
 #' @param Y_test response for the validation sample
 #' @param intercept boolean. If FALSE intercept will be set to 0 in each model.
 #' @param alpha Coefficients of the explicative model to coerce the predictive step. if not NULL explicative step is not computed.
-#' @param prednew alternate optimisation for predictive
-#' @param nbalter number of alternance for prednew
-#' @param deltamin criterion to stop alternance 
 #' @param g number of group of variables for clere
-#' @param explnew select the number of sub-regression to take into account (by AIC on the corresponding final model)
 #' 
 #Attention cette fonction dégage une puissance phénoménale (it's over 9000!)
 correg<-function (X = X, Y = Y, Z = NULL, B = NULL, compl = TRUE, expl = FALSE, explnew=FALSE,
-                pred = FALSE,prednew=FALSE,
+                pred = FALSE,
                 select = "lar",
                 criterion = c("MSE", "BIC"),
                 X_test = NULL, Y_test = NULL, intercept = TRUE, 
                 K = 10, groupe = NULL, Amax = NULL, lambda = 1,returning=FALSE,final=FALSE,
-                nbalter=10,deltamin=0.01,alpha=NULL,g=5) 
+                alpha=NULL,g=5) 
 {
   res = list()
   X = 1*as.matrix(X)
@@ -184,92 +180,7 @@ if(explnew){compl=TRUE;expl=TRUE}
     res$expl$BIC = BicTheta(X = X, Y = Y, intercept = intercept, beta = A_expl)
     res$expl$AIC=mon_AIC(theta=res$expl$A,Y=Y,X=X,intercept=intercept) 
     
-    #nouvel explicatif
-    if(explnew){#selection sur explicatif####
-      qui = WhoIs(Z = Z)
-      I1 = qui$I1
-      I2 = qui$I2
-      R2_vect=R2Z(Z=Z,X=X,adj=TRUE)#evaluation des R2
-      R2_vect=R2_vect[R2_vect!=0]
-      #tri par R2 => nouvel I2 ordonné
-      I2=I2[order(R2_vect,decreasing=FALSE)]#on commence par garder ce qui est mal expliqué par le reste (donc forte perte et peu de corrélations)
-      #initialisation du résultat final (par le modèle complet déjà calculé, + AIC associé)
-      res$expl2$A=res$expl$A
-      resopt=res$expl$A
-      AICopt=mon_AIC(theta=resopt,Y=Y,X=X,intercept=intercept)
-      for (iexplnew in 1:length(I2)){
-         I1=c(qui$I1,I2[1:iexplnew])#c'est ici que tout se joue
-          if(!is.null(alpha)){
-             res$expl2$A=alpha
-          }else if (select == "NULL") {
-             res$expl2$A = OLS(X = as.matrix(X[, I1]), Y = Y, intercept = intercept)$beta
-          }else if (select != "elasticnet" & select != "ridge" & select != "adalasso"  & select!="clere" & select!="spikeslab") {
-             lars_expl = lars(x = as.matrix(X[, I1]), y = Y, type = select, 
-                              intercept = intercept)
-             res$expl2 = meilleur_lars(lars = lars_expl, X = as.matrix(X[, I1]), Y = Y, 
-                                      mode = criterion, intercept = intercept, 
-                                      K = K, groupe = groupe, Amax = Amax)
-          }else if (select=="elasticnet"){
-             lars_expl = renet(x = as.matrix(X[, I1]), y = Y, intercept = intercept, 
-                               lambda = lambda)
-             names(lars_expl)[4] = "coefficients"
-             res$expl2 = meilleur_lars(lars = lars_expl, X = as.matrix(X[,I1]), Y = Y, 
-                                      mode = criterion, intercept = intercept, 
-                                      K = K, groupe = groupe, Amax = Amax)
-          }else if(select=="adalasso"){
-             resada=parcor::adalasso(X=X[,I1],y=Y,k=K)    
-             if(intercept){
-                if(is.null(resada$intercept.adalasso)){
-                   resada$intercept.adalasso=0
-                }
-                res$expl2$A=c(resada$intercept.adalasso,resada$coefficients.adalasso)
-             }else{
-                res$expl2$A=c(resada$coefficients.adalasso)
-             }
-             Xloc=X[,I1][,resada$coefficients.adalasso!=0]
-             res$expl2$A[res$expl2$A!=0]=c(OLS(X=Xloc,Y=Y,intercept=intercept)$beta)       
-          }else if(select=="clere"){
-             res$expl2$A=A_clere(y=as.numeric(Y),x=X[,I1],g=g)
-          }else if(select=="spikeslab"){
-             respike=spikeslab::spikeslab(x=X[,I1],y=Y)
-             res$expl2$A=rep(0,times=ncol(X[,I1])+intercept)
-             if(intercept){
-                res$expl2$A[c(1,1+which(respike$gnet.scale!=0))]=OLS(X=X[,I1][,respike$gnet.scale!=0],Y=as.numeric(Y),intercept=intercept)$beta
-             }else{
-                res$expl2$A[respike$gnet.scale!=0]=OLS(X=X[,I1][,respike$gnet.scale!=0],Y=as.numeric(Y),intercept=intercept)$beta
-             }
-          }else{#ridge
-             lars_expl = linearRidge(Y~.,data=data.frame(X[,I1]))
-             res$expl2$A=coef(lars_expl)
-          }
-          if(is.null(alpha)){
-             A_expl = rep(0, times = ncol(X) + intercept)
-             if(intercept){
-                A_expl[c(intercept, I1 + intercept)] = res$expl2$A
-             }else{
-                A_expl[I1] = res$expl2$A
-             }
-             res$expl2$A = A_expl
-          }else{
-             A_expl=alpha
-          }
-          res$expl2$BIC = BicTheta(X = X, Y = Y, intercept = intercept, beta = A_expl)
-         #calcul du AIC
-         AICloc=mon_AIC(theta=res$expl2$A,Y=Y,X=X,intercept=intercept)
-         if(AICloc<AICopt){
-            resopt=res$expl2$A
-            AICopt=AICloc
-         }
-      }
-      #on remet les choses à leur place
-      res$expl2$A=resopt
-      res$expl2$AIC=AICopt
-      res$expl2$BIC = BicTheta(X = X, Y = Y, intercept = intercept, 
-                              beta = res$expl2$A)
-      qui = WhoIs(Z = Z)
-      I1 = qui$I1
-      I2 = qui$I2
-    }#fin du nouvel explicatif
+
 #predictif
     if (pred & length(I2)>0) {     
       if (is.null(B)) {
@@ -378,155 +289,12 @@ if(explnew){compl=TRUE;expl=TRUE}
           A_pred[ I1 ] = A_expl[I1] - B[c(1, I1 + 1), I2] %*% as.matrix(A_inj)
         }
         
-      }
-         
+      }    
       res$pred$A = A_pred
       res$pred$CVMSE = CVMSE(X = X[, which(A_pred[-1] != 0)], Y = Y, intercept = intercept, K = K, groupe = groupe)
-      res$pred$BIC = BicTheta(X = X, Y = Y, intercept = intercept, 
-                              beta = A_pred)
+      res$pred$BIC = BicTheta(X = X, Y = Y, intercept = intercept, beta = A_pred)
       res$pred$AIC=mon_AIC(theta=res$pred$A,Y=Y,X=X,intercept=intercept) 
       
-    }
-    #nouveau prédictif####
-    if (prednew) {
-       if(length(I2)<2 & select=="adalasso"){
-          select="lar"
-       }
-       if (is.null(B)) {
-          B = hatB(Z = Z, X = X)
-       }
-       nbit=0
-       deltaobs=deltamin+1
-       A_old=rep(0,times=(ncol(Z)+intercept))
-       while(nbit<nbalter & deltaobs>deltamin){
-          nbit=nbit+1
-          #on fait tout comme d'hab
-             Xtilde = X[, I2] - cbind(rep(1, times = nrow(X)), X[, I1]) %*% B[c(1, I1 + 1), I2]
-             Xtilde = as.matrix(Xtilde)
-             if(intercept){
-                Ytilde = Y - as.matrix(X[, I1]) %*% A_expl[-1][I1] - A_expl[1]
-             }else{
-                Ytilde = Y - as.matrix(X[, I1]) %*% A_expl[I1]
-             }
-             if (select == "NULL") {
-                A_inj = OLS(X = Xtilde, Y = Ytilde, intercept = F)$beta
-             }else if (select != "elasticnet"  & select != "ridge" & select != "adalasso" & select!="clere" & select!="spikeslab") {
-                lars_inj = lars(x = Xtilde, y = Ytilde, type = select, 
-                                intercept = F)
-                A_inj = meilleur_lars(lars = lars_inj, X = Xtilde, 
-                                      Y = Ytilde, mode = criterion, intercept = F, 
-                                      K = K, groupe = groupe)$A
-             }else if (select=="elasticnet") {
-                lars_inj = renet(x = Xtilde, y = Ytilde, intercept = F, 
-                                 lambda = lambda)
-                names(lars_inj)[4] = "coefficients"
-                A_inj = meilleur_lars(lars = lars_inj, X = Xtilde, 
-                                      Y = Ytilde, mode = criterion, intercept = F, 
-                                      K = K, groupe = groupe)$A
-             }else if(select=="adalasso"){
-                resada=parcor::adalasso(X=Xtilde,y=Ytilde,k=K)    
-                A_inj=c(resada$coefficients.adalasso)
-                if(length(A_inj[A_inj!=0])>0){
-                   Xloc=Xtilde[,A_inj!=0]
-                   A_inj[A_inj!=0]=c(OLS(X=Xloc,Y=Y,intercept=F)$beta)
-                }  
-             }else{#ridge
-                ridge_pred = linearRidge(Ytilde~0+.,data=data.frame(Xtilde))
-                A_inj=coef(ridge_pred)
-             }
-             A_pred = rep(0, times = ncol(X) + intercept)
-             A_pred[I2 + intercept] = A_inj
-             if(returning){
-                Ytildebis=Y-as.matrix(X[,I2])%*%A_pred[I2 + intercept]
-                Ytildebis=as.matrix(Ytildebis)
-                if (select != "elasticnet" & select != "ridge" & select != "adalasso" & select!="clere" & select!="spikeslab") {
-                   lars_retour=lars(x = as.matrix(X[,I1]), y = Ytildebis, type = select, 
-                                    intercept = intercept)
-                   A_retour = meilleur_lars(lars = lars_retour, X = as.matrix(X[,I1]), 
-                                            Y = Ytildebis, mode = criterion, intercept = intercept, 
-                                            K = K, groupe = groupe)$A
-                }else if (select=="elasticnet"){
-                   lars_retour= renet(x = as.matrix(X[,I1]), y = Ytildebis, intercept =intercept, 
-                                      lambda = lambda)
-                   names(lars_retour)[4] = "coefficients"
-                   A_retour = meilleur_lars(lars = lars_retour, X = as.matrix(X[,I1]), 
-                                            Y = Ytildebis, mode = criterion, intercept = intercept, 
-                                            K = K, groupe = groupe)$A
-                }else if(select=="adalasso"){
-                   resada=parcor::adalasso(X=X[,I1],y=Ytildebis,k=K)    
-                   if(intercept){
-                      A_retour=c(resada$intercept.adalasso,resada$coefficients.adalasso)
-                   }else{
-                      A_retour=c(resada$coefficients.adalasso)
-                   }
-                   Xloc=X[,I1][,resada$coefficients.adalasso!=0]
-                   A_retour[A_retour!=0]=c(OLS(X=Xloc,Y=Y,intercept=intercept)$beta)   
-                }else{#ridge
-                   ridge_pred = linearRidge(Ytildebis~.,data=data.frame(X[,I1]))
-                   A_retour=coef(ridge_pred)
-                }
-                
-                if(intercept){
-                   A_pred[c(intercept, I1 + intercept)] =A_retour
-                }else{
-                   A_pred[ I1 ] = A_retour
-                }
-             }else{
-                if(intercept){
-                   A_pred[c(intercept, I1 + intercept)] = A_expl[c(intercept,I1 + intercept)]- B[c(1, I1 + 1), I2] %*% as.matrix(A_inj)
-                }else{
-                   A_pred[ I1 ] = A_expl[I1]-B[c(1, I1 + 1), I2] %*% as.matrix(A_inj)
-                }
-                
-             }
-             
-             res$prednew$A = A_pred
-             res$prednew$CVMSE = CVMSE(X = X[, which(A_pred[-1] != 0)], Y = Y, intercept = intercept, K = K, groupe = groupe)
-             res$prednew$BIC = BicTheta(X = X, Y = Y, intercept = intercept, 
-                                     beta = A_pred)
-#              if(final){
-#                 if(intercept){
-#                    quifinal=which(A_pred[-1]!=0)
-#                 }else{
-#                    quifinal=which(A_pred!=0)
-#                 }
-#                 Zfinal=as.matrix(Z[quifinal,quifinal])
-#                 A_final=correg(X=X[,quifinal],Y=Y,Z=Zfinal,B=as.matrix(B[c(1,quifinal+1),quifinal]),returning=F,final=F,groupe=groupe,K=K,intercept=intercept,criterion=criterion,select="NULL")$pred$A
-#                 
-#                 res$finalnew$A=res$prednew$A
-#                 res$finalnew$A[res$prednew$A!=0]=A_final
-#                 res$finalnew$CVMSE = CVMSE(X = as.matrix(X[,quifinal]), Y = Y, intercept = intercept, K = K, groupe = groupe)
-#                 res$finalnew$BIC = BicTheta(X = as.matrix(X[,quifinal]), Y = Y, intercept = intercept, 
-#                                          beta = A_final)
-#              }
-          #une fois A calculé, on regarde si on doit continuer
-          deltaobs=sum(abs(A_old-res$prednew$A))
-          print(deltaobs)
-          if(!is.nan(deltaobs)){
-             if(deltaobs>deltamin & nbit<nbalter){#on continue
-                #on estime un nouveau B
-                A_old=res$prednew$A
-                newB=newhatB(Z=Z,A=res$prednew$A,Atilde=res$expl$A,Bold=B,intercept=intercept)
-                if(prod(B==newB)){#rien n'a change dans B
-                   nbit=nbalter+1
-                   print("same B : stop")
-                }else if(any(newB==Inf)){
-                   nbit=nbalter+1
-                   print("numerical explosion of B: stop")
-                   res$prednew$A=A_old
-                }else{
-                   B=newB;print(paste("new B",nbit))
-                }
-             }
-          }else{
-             nbit=nbalter+1
-             print("numerical explosion : stop")
-             res$prednew$A=A_old
-          }
-          
-          
-       } #fin de la boucle d'optimisation alternee 
-       res$B=B
     }
   }else if (sum(Z) == 0 & (expl | pred)) {
     res$expl$A = res$compl$A
