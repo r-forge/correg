6ce08bbe04b719f113355b20e78a8fa461b778f9
clementthery (Clément Théry) <clement.thery@arcelormittal.com>
2015-02-12 21:22:28 +0000

    up
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@1050 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/showdata.R
+++ pkg/CorReg/R/showdata.R
@@ -2,14 +2,15 @@
 #'
 #'@param X the dataset to analyse
 #'@param what indicates what to plot
+#'@param pch for missing, symbol to plot (can set pch="." for large datasets)
 #'@export
 
-showdata<-function(X=X,what=c("miss","correl")){
+showdata<-function(X=X,what=c("miss","correl"),pch=7){
    what=what[1]
    if(what=="miss"){
       M=which(is.na(X),arr.ind=T)
       if(nrow(M)>1){
-         plot(M[,c(2,1)],pch=7)
+         plot(M[,c(2,1)],pch=pch)
          title("Missing values in the dataset")  
       }else{
          print("No missing values")
