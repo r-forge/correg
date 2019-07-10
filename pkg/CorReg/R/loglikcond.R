91bf4b0fbd7ef219fba8c48af182d72caf65567d
clementthery <clementthery@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-07-10 08:57:09 +0000

    update to CRAN 1.2.8
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/correg@1075 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/CorReg/R/loglikcond.R
+++ pkg/CorReg/R/loglikcond.R
@@ -2,7 +2,7 @@
 # ' X is the entire dataset (const &)
 
 loglikcond<-function(X=X,mui=mui,Sigma=Sigma,M=M,i=i,Zc=Zc){ 
-   res=mvtnorm::dmvnorm(x=X[i,],mean=mui,sigma=Sigma)#vraisemblance globale de l'individu
+   res=dmvnorm(x=X[i,],mean=mui,sigma=Sigma)#vraisemblance globale de l'individu
    quimank_i=which(M[i,]==1)
    if(length(quimank_i)>0){
       Sigma_M_obs=matrix(Sigma[quimank_i,-quimank_i],nrow=length(quimank_i))
@@ -10,7 +10,7 @@ loglikcond<-function(X=X,mui=mui,Sigma=Sigma,M=M,i=i,Zc=Zc){
       prodmat=Sigma_M_obs%*%solve(Sigma_obs_obs)
       mu=mui[quimank_i]+prodmat%*%(X[i,-quimank_i]-mui[-quimank_i])
       sigma=Sigma[quimank_i,quimank_i]-prodmat%*%t(Sigma_M_obs)#matrice variance-covariance
-      res=res/mvtnorm::dmvnorm(x=X[i,quimank_i],mean = mu,sigma=sigma)#on divise par la vraisemblance conditionnelle des manquants pour obtenir la marginale
+      res=res/dmvnorm(x=X[i,quimank_i],mean = mu,sigma=sigma)#on divise par la vraisemblance conditionnelle des manquants pour obtenir la marginale
       #attention sigma minuscule dans les deux lignes precedentes  
    }
    return(log(res))
